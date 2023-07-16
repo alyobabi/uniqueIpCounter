dbe7640482082b11987faf48466fd0e3b049d843
alena <alyobabi@gmail.com>
2023-07-16 23:49:09 +0400

    updated test files; removed redundant lines

--- src/main/java/org/example/UniqueIpAddressCounter.java
+++ src/main/java/org/example/UniqueIpAddressCounter.java
@@ -1,15 +1,13 @@
 package org.example;
 
 import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.BitSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.Logger;
 
 public class UniqueIpAddressCounter {
-
-    public static Logger LOG = Logger.getLogger(UniqueIpAddressCounter.class.getName());
 
     private static final int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
 
@@ -18,15 +16,13 @@ public class UniqueIpAddressCounter {
         BitSet negativeHashCodeSet = new ConcurrentBitSet();
 
         try (ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE)) {
-            try (RandomAccessFile file = new RandomAccessFile(fileName, "r")) {
-                long fileSize = file.length();
-                long chunkSize = (long) Math.ceil((double) fileSize / THREAD_POOL_SIZE);
-                for (int i = 0; i < THREAD_POOL_SIZE; i++) {
-                    long startOffset = i * chunkSize;
-                    long endOffset = (i + 1) * chunkSize;
-                    executorService.execute(new UniqueIpCounterThread(fileName, startOffset, endOffset,
-                            positiveHashCodeSet, negativeHashCodeSet));
-                }
+            long fileSize = Files.size(Path.of(fileName));
+            long chunkSize = (long) Math.ceil((double) fileSize / THREAD_POOL_SIZE);
+            for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+                long startOffset = i * chunkSize;
+                long endOffset = (i + 1) * chunkSize;
+                executorService.execute(new UniqueIpCounterThread(fileName, startOffset, endOffset,
+                        positiveHashCodeSet, negativeHashCodeSet));
             }
         }
         return negativeHashCodeSet.cardinality() + positiveHashCodeSet.cardinality();
